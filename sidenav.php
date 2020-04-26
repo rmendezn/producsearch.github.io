c3d96df76d0270c19d3d8555d4f4797a86eb8b92
rmendezn <ramon.mendez.nava@gmail.com>
2020-04-26 10:13:45 -0600

    segundo

---
+++ sidenav.php
@@ -0,0 +1,8 @@
+<!-- Side Navigation -->
+<nav class="w3-sidebar w3-bar-block w3-card w3-animate-left w3-center" style="display:none" id="mySidebar">
+  <h1 class="w3-xxxlarge w3-text-theme">Side Navigation</h1>
+  <button class="w3-bar-item w3-button" onclick="w3_close()">Close <i class="fa fa-remove"></i></button>
+  <a href="search.php" class="w3-bar-item w3-button">Search</a>
+  <a href="#" class="w3-bar-item w3-button">Orders</a>
+  <a href="#" class="w3-bar-item w3-button">Locations</a>
+</nav>
