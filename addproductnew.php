f40c57e0aef959acfa18f1dccad754d146007f8a
rmendezn <ramon.mendez.nava@gmail.com>
2020-04-29 19:22:06 -0600

    dia 2

--- addproductnew.php
+++ addproductnew.php
@@ -5,10 +5,10 @@ $product=$_POST['product'];
 $code=$_POST['code'];
 $position=$_POST['position'];
 
-// echo $row;
-// echo $rack;
-// echo $shelf;
-// echo $position;
+//echo $row;
+//echo $rack;
+//echo $shelf;
+//echo $position;
 
 $conn=conexion();
   $sql = "SELECT * FROM product WHERE nameProduct = '$product'";
@@ -20,7 +20,7 @@ if($count==1){
 	echo 2;
 }else{
 	$conn1=conexion();
-	  $sql1 = "INSERT INTO `product` (`id_product`,`nameProduct`, `codeProduct`) VALUES (NULL, '$product','$code')";
+	  echo $sql1 = "INSERT INTO `product` (`id_product`,`nameProduct`, `codeProduct`) VALUES (NULL, '$product','$code')";
 	  $sqlquery1 = mysqli_query($conn1,$sql1);
 	mysqli_close($conn1);
 
@@ -35,8 +35,8 @@ if($count==1){
 	$id_loc = "0".$position;
 
 	$conn3=conexion();
-	  $sql3 = "INSERT INTO `relprodloc` (`id_rel`, `id_product`, `id_location`) VALUES (NULL, '$id_prod', '$id_loc')";
-	  $sqlquery3 = mysqli_query($conn3,$sql3);
+	  echo $sql3 = "INSERT INTO `relprodloc` (`id_rel`, `id_product`, `id_location`) VALUES (NULL, '$id_prod', '$id_loc')";
+	  //$sqlquery3 = mysqli_query($conn3,$sql3);
 	mysqli_close($conn3);
 
 	echo 1;
